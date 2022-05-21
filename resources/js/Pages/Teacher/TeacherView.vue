9e33c886b4fd0f8027c4213eea00218678112302
mohamedkhaled2000 <mohhaaaak4@gmail.com>
2022-05-22 01:29:20 +0200

    gg

--- resources/js/Pages/Teacher/TeacherView.vue
+++ resources/js/Pages/Teacher/TeacherView.vue
@@ -60,7 +60,7 @@
                         </div>
                         <div class="card-body">
 
-                            <div class="table-responsive">
+                            <div class="table-responsive-sm">
 
                                 <table class="table">
                                             <thead>
